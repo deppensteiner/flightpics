fa2d5dd90e4f55fafc7908be53d8653e7321379d
Daniel Eppensteiner <danieleppensteiner@Daniels-MacBook-Pro.local>
2018-12-14 15:41:48 +0100

    - added loading component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,7 @@ import Icon from './Icon';
 class Footer extends Component {
   render() {
     return (
-      <div className="Footer">
-        <Icon id="plane" className="footer-plane-icon" />
-      </div>
+      <div className="Footer" />
     );
   }
 }
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Icon from './Icon';
+
+import './css/loading.css';
 
 const Loading = () => {
   return (
-    <div>
-      Loading
+    <div className="circular_blue">
+      <div className="style1">
+        <div className="style2">
+          <Icon id="plane" className="footer-plane-icon style3" />
+        </div>
+      </div>
     </div>
   )  
 }
